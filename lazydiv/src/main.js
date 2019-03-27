c7f120c49eef750a36a59060e5f34b123f70b4da
w ww <956826374@qq.com>
2019-03-27 19:30:35 +0800

    updata

--- lazydiv/src/main.js
+++ lazydiv/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import LazyDiv from './index.js'
-import LazyBackground from '../../../../wuyang/vue-lazy-background-component/dist/bundle'
+import LazyBackground from 'vue-lazy-background-component'
+// import LazyBackground from '../../../../wuyang/vue-lazy-background-component/dist/bundle'
 
-console.log(LazyBackground)
 // Vue.use(LazyDiv)
 Vue.use(LazyBackground)
 new Vue({
